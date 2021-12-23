d662061f64584ff8997535520393003b2f331fd0
dev <dev@devs.local>
2021-12-23 04:01:19 +0300

    -

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -1,82 +1,66 @@
-#!/usr/bin/python3
-""" 5. LFU Caching
-"""
+#!/usr/bin/env python3
 
-from enum import Enum
-from heapq import heappush, heappop
-from itertools import count
+''' LFU Caching: Create a class LFUCache that inherits from BaseCaching
+                 and is a caching system '''
 
-BaseCaching = __import__("base_caching").BaseCaching
-
-
-class HeapItemStatus(Enum):
-    """ HeapItemStatus class.
-    """
-    ACTIVE = 1
-    INACTIVE = 2
+BaseCaching = __import__('base_caching').BaseCaching
 
 
 class LFUCache(BaseCaching):
-    """ Create a class LFUCache that inherits from BaseCaching and is a caching
-    system
-    """
+    ''' An LFU cache.
+        Inherits all behaviors from BaseCaching except, upon any attempt to add
+        an entry to the cache when it is at max capacity (as specified by
+        BaseCaching.MAX_ITEMS), it discards the least frequently used entry to
+        accommodate for the new one.
+        Attributes:
+          __init__ - method that initializes class instance
+          put - method that adds a key/value pair to cache
+          get - method that retrieves a key/value pair from cache '''
 
     def __init__(self):
-        """ Init
-        """
+        ''' Initialize class instance. '''
         super().__init__()
-        self.heap = []
-        self.map = {}
-        self.counter = count()
+        self.keys = []
+        self.uses = {}
 
     def put(self, key, item):
-        """ Must assign to the dictionary self.cache_data the item value for
-        the key key
-        """
-        if key and item:
-            if key in self.cache_data:
-                self.rehydrate(key)
-            else:
-                if self.is_full():
-                    self.evict()
-                self.add_to_heap(key)
+        ''' Add key/value pair to cache data.
+            If cache is at max capacity (specified by BaseCaching.MAX_ITEMS),
+            discard least frequently used entry to accommodate new entry. '''
+
+        if key is not None and item is not None:
+            if (len(self.keys) == BaseCaching.MAX_ITEMS and
+                    key not in self.keys):
+                discard = self.keys.pop(self.keys.index(self.findLFU()))
+                del self.cache_data[discard]
+                del self.uses[discard]
+                print('DISCARD: {:s}'.format(discard))
             self.cache_data[key] = item
+            if key not in self.keys:
+                self.keys.append(key)
+                self.uses[key] = 0
+            else:
+                self.keys.append(self.keys.pop(self.keys.index(key)))
+                self.uses[key] += 1
 
     def get(self, key):
-        """ Must return the value in self.cache_data linked to key.
-        """
-        if key in self.cache_data:
-            self.rehydrate(key)
-            return self.cache_data.get(key)
+        ''' Return value stored in `key` key of cache.
+            If key is None or does not exist in cache, return None. '''
+        if key is not None and key in self.cache_data:
+            self.keys.append(self.keys.pop(self.keys.index(key)))
+            self.uses[key] += 1
+            return self.cache_data[key]
+        return None
 
-    def is_full(self):
-        """ If the number of items in self.cache_data is higher that
-        BaseCaching.MAX_ITEMS
-        """
-        return len(self.cache_data) >= self.MAX_ITEMS
+    def findLFU(self):
+        ''' Return key of least frequently used item in cache.
+            If multiple items have the same amount of uses, return the least
+            recently used one. '''
+        items = list(self.uses.items())
+        freqs = [item[1] for item in items]
+        least = min(freqs)
 
-    def evict(self):
-        """ you must print DISCARD: with the key discarded and following by a
-        new line
-        """
-        while self.heap:
-            _, __, item, status = heappop(self.heap)
-            if status == HeapItemStatus.ACTIVE:
-                print("DISCARD: " + str(item))
-                del self.cache_data[item]
-                return
-
-    def rehydrate(self, key):
-        """ Marks current item as inactive and reinserts updated count back
-        into heap.
-        """
-        entry = self.map[key]
-        entry[-1] = HeapItemStatus.INACTIVE
-        self.add_to_heap(key, entry[0])
-
-    def add_to_heap(self, key, count=0):
-        """ Adds a new entry into heap.
-        """
-        entry = [1 + count, next(self.counter), key, HeapItemStatus.ACTIVE]
-        self.map[key] = entry
-        heappush(self.heap, entry)
+        lfus = [item[0] for item in items if item[1] == least]
+        for key in self.keys:
+            if key in lfus:
+                return key
